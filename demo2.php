388a42abb66213d5ba39a3722828f397270b77a7
Julian Brown <brownjulian2@gmail.com>
2014-07-16 20:55:29 -0400

    clean up

--- demo2.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>MMC</title>
-  </head>
-  <body>
-<img class="poster" src="http://ia.media-imdb.com/images/M/MV5BNjE5MzYwMzYxMF5BMl5BanBnXkFtZTcwOTk4MTk0OQ@@._V1_SX300.jpg" />
-</body>
-</html>
