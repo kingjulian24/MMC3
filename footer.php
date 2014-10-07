6c62c8aa922fd7024224e238ee5b243dbdf4297c
Julian Brown <brownjulian2@gmail.com>
2014-10-07 13:51:59 -0400

    init

--- footer.php
+++ footer.php
@@ -1,7 +1,25 @@
 	</div> <!-- end of container -->
+        <script type='text/javascript'>
+            var $container = $('#container').isotope({
+              getSortData: {
+                name: '.title', // text from querySelector
+                number: '.price', // value of attribute
+                avail: '.available'
+              }
+            });
+            
+            
+                // sort items on button click
+                $('#sort').on( 'click', 'th', function() {
+                  var sortByValue = $(this).attr('title');
+                  $container.isotope({ sortBy: 'name' });
+                });
+            
+        </script>
 	<script src="http://code.jquery.com/jquery.js"></script>
    <!-- Latest compiled and minified JavaScript -->
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="assets/javascripts/my-js.js"></script>
+    <script src="assets/javascripts/isotope.js"></script>
   </body>
 </html>
